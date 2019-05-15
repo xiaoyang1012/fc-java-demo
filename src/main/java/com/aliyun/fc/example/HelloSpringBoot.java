06af861fd1d70dd531767770580c57db858e817d
santihong <591667782@qq.com>
2019-05-15 16:46:51 +0800

    Add springboot demo

--- src/main/java/com/aliyun/fc/example/HelloSpringBoot.java
+++ src/main/java/com/aliyun/fc/example/HelloSpringBoot.java
@@ -13,16 +13,14 @@ import com.aliyun.fc.runtime.FunctionInitializer;
 import com.aliyun.fc.runtime.HttpRequestHandler;
 
 
-public class HelloWebLocal implements FunctionInitializer, HttpRequestHandler {
+public class HelloSpringBoot implements FunctionInitializer, HttpRequestHandler {
     private FcAppLoader fcAppLoader = new FcAppLoader();
 
-    private String key = "greenhouse.war";
- 
-    // Not use custom domain
-    // private String userContextPath = "/2016-08-15/proxy/${YourServiceName}/${YourFunctionName}";
+    private String ossEndPoint = "${YourOSSEndPoint}";
+    private String bucket = "{YourOSSBucket}";
+    private String key = "demo-springboot-hello-1.0.0.war";
     
-    // Use custom domain
-    private String userContextPath = "/greenhouse";
+    private String userContextPath = "/2016-08-15/proxy/fun-java/demo-springboot";
     
     @Override
     public void initialize(Context context) throws IOException {
@@ -30,17 +28,22 @@ public class HelloWebLocal implements FunctionInitializer, HttpRequestHandler {
         
         fcAppLoader.setFCContext(context);
         
-        // Set war file path
-        fcAppLoader.loadCodeFromLocalProject(key);
+        // Load code from OSS
+        fcLogger.info("Begin load code: " + key);
+        boolean codeSuccess = fcAppLoader.loadCodeFromOSS(ossEndPoint, bucket, key);
+        if (! codeSuccess) {
+            throw new IOException("Download code failed");
+        }
+        fcLogger.info("End load code");
         
         // Init webapp from code
         long timeBegin = System.currentTimeMillis();
-        fcLogger.info("Loading webapp: " + key);
-        boolean initSuccess = fcAppLoader.initApp(userContextPath, HelloWebLocal.class.getClassLoader());
+        fcLogger.info("Begin load webapp");
+        boolean initSuccess = fcAppLoader.initApp(userContextPath, HelloSpringBoot.class.getClassLoader());
         if(! initSuccess) {
             throw new IOException("Init web app failed");
         }
-        fcLogger.info("Loaded webapp, elapsed: " + (System.currentTimeMillis() - timeBegin) + "ms");
+        fcLogger.info("End load webapp, elapsed: " + (System.currentTimeMillis() - timeBegin) + "ms");
     }
     
     @Override
